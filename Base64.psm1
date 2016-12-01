3162d05fc3b607c1fe3a46609b7df62e1d5a38f9
Frank Peter Schultze <fpschultze@live.de>
2016-12-01 11:03:29 +0100

    Ready

---
+++ Base64.psm1
@@ -0,0 +1,93 @@
+﻿# Add 'ToBase64String' method to System.String
+Update-TypeData -TypeName System.String -MemberType ScriptMethod -MemberName ToBase64String -Value {
+  $ErrorActionPreference = 'Stop'
+  try
+  {
+    [System.Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($this))
+  }
+  catch
+  {
+    $_.Exception.Message | Write-Warning
+  }
+}
+
+# Add 'FromBase64String' method to System.String
+Update-TypeData -TypeName System.String -MemberType ScriptMethod -MemberName FromBase64String -Value {
+  $ErrorActionPreference = 'Stop'
+  try
+  {
+    [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($this))
+  }
+  catch
+  {
+    $_.Exception.Message | Write-Warning
+  }
+}
+
+function ConvertTo-Base64String
+{
+  <#
+    .SYNOPSIS
+    Converts the given text to a Base64 string.
+
+    .EXAMPLE
+    $encodedCommand = ConvertTo-Base64String 'Get-ChildItem $env:TEMP | ?{$_.LastWriteTime -lt (Get-Date).AddDays(-1)}'
+    This example creates a Base64 encoded powershell command. This can be used with powershell.exe's -encodedcommand parameter.
+
+    .EXAMPLE
+    'Get-ChildItem $env:TEMP | ?{$_.LastWriteTime -lt (Get-Date).AddDays(-1)}' | ConvertTo-Base64String
+
+    .INPUTS
+    System.String
+
+    .OUTPUTS
+    System.String
+  #>
+
+  [OutputType([String])]
+  param
+  (
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $Text
+  )
+  process
+  {
+    $Text.ToBase64String()
+  }
+}
+
+function ConvertFrom-Base64String
+{
+  <#
+    .SYNOPSIS
+    Converts the given Base64 encoded text to the original text.
+
+    .EXAMPLE
+    $Command = ConvertFrom-Base64String $encodedCommand
+    This example reverse engineers a previously encoded text.
+
+    .EXAMPLE
+    $encodedCommand | ConvertFrom-Base64String
+
+    .INPUTS
+    System.String
+
+    .OUTPUTS
+    System.String
+  #>
+
+  [OutputType([String])]
+  param
+  (
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $Text
+  )
+  process
+  {
+    $Text.FromBase64String()
+  }
+}
---
+++ Tests/Base64.tests.ps1
@@ -0,0 +1,27 @@
+﻿$ModuleRoot = Split-Path -Parent $PSScriptRoot
+$ModuleFile = (Split-Path -Leaf $PSCommandPath) -replace '\.tests\.ps1$', '.psm1'
+Import-Module "$ModuleRoot\$ModuleFile"
+
+$TestText = 'Get-ChildItem $env:TEMP | ?{$_.LastWriteTime -lt (Get-Date).AddDays(-1)}'
+$TestBase64 = 'RwBlAHQALQBDAGgAaQBsAGQASQB0AGUAbQAgACQAZQBuAHYAOgBUAEUATQBQACAAfAAgAD8AewAkAF8ALgBMAGEAcwB0AFcAcgBpAHQAZQBUAGkAbQBlACAALQBsAHQAIAAoAEcAZQB0AC0ARABhAHQAZQApAC4AQQBkAGQARABhAHkAcwAoAC0AMQApAH0A'
+
+Describe 'ConvertTo-Base64String' {
+  Context 'Running with arguments' {
+    It 'does return Base64 encoded text' {
+      ConvertTo-Base64String $TestText | Should Be $TestBase64
+    }
+  }
+}
+
+Describe 'ConvertFrom-Base64String' {
+  Context 'Running with valid arguments' {
+    It 'does return the original text' {
+      ConvertFrom-Base64String $TestBase64 | Should Be $TestText
+    }
+  }
+  Context 'Running with non-base64 arguments' {
+    It 'does not return anything and displays warning' {
+      ConvertFrom-Base64String $TestText | Should BeNullOrEmpty
+    }
+  }
+}
